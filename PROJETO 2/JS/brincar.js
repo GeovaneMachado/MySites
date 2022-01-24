2ce35fa790d6a94fe041c157b3162d4e93b0e106
GeovaneMachado <geovanemachado9@gmail.com>
2022-01-24 19:57:56 -0300

    calculadora

--- PROJETO 2/JS/brincar.js
+++ PROJETO 2/JS/brincar.js
@@ -1,27 +1,38 @@
-function soma(a, b){
-    document.write(Number(a+b))
-    return Number(a + b)
+function insert(num){
+    var numero = document.getElementById('Resultado').innerHTML;
+    document.getElementById('Resultado').innerHTML = numero + num;
 }
-function diminui(a, b){
-    document.write(Number(a-b))
-    return Number(a - b)
+clean = () => document.getElementById('Resultado').innerHTML = "";
+back = () => {
+    var result = document.getElementById('Resultado').innerHTML;
+    document.getElementById('Resultado').innerHTML = result.substring(0, result.length -1) 
 }
-function multiplica(a, b){
-    document.write(Number(a*b))
-    return Number(a * b)
-}
-function dividi(a, b){
-    document.write(Number(a/b))
-    return Number(a/b)
-}
-/*
-var numeros ={
-    'numero1': '7', 
-    'numero2': '5'
-}
-var {'numero1':a, 'numero2': b} = numeros
 
-console.log("Resultado soma = ", soma(a, b) )
-console.log("Resultado subtração = ", diminui(a, b) )
-console.log("Resultado multiplicação = ", multiplica(a, b) )
-console.log("Resultado divisão = ", dividi(a, b) )*/
+resultado = () => {
+    var resultado = document.getElementById("Resultado").innerHTML;
+    if(resultado){
+        document.getElementById("Resultado").innerHTML = eval(resultado)
+    }
+    else{
+        alert("NADA PARA CALCULAR")
+    }
+}
+
+function keyPressed(evt){
+    evt = evt || window.event;
+    var key = evt.keyCode || evt.which;
+    return String.fromCharCode(key); 
+}
+
+document.onkeypress = function(evt) {
+    var str = keyPressed(evt);
+    const tecla = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '+', '-', '/', '*']
+    for(let i = 0; i < tecla.length; i++) 
+        if(str == tecla[i]) 
+            insert(str);
+    if(str == "="){
+        resultado();
+    }
+    else if(str == "c") clean();
+    else if(str == "<") back();
+};
